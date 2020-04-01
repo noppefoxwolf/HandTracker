609115f83815f4ea3761c9b381277410e67badb3
noppefoxwolf <noppelabs@gmail.com>
2020-04-02 03:50:00 +0900

    Add Example

---
+++ Example/Example/Camera.swift
@@ -0,0 +1,37 @@
+//
+//  Camera.swift
+//  Example
+//
+//  Created by Tomoya Hirano on 2020/04/02.
+//  Copyright © 2020 Tomoya Hirano. All rights reserved.
+//
+
+import AVFoundation
+
+class Camera: NSObject {
+    lazy var session: AVCaptureSession = .init()
+    lazy var input: AVCaptureDeviceInput = try! AVCaptureDeviceInput(device: device)
+    lazy var device: AVCaptureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front)!
+    lazy var output: AVCaptureVideoDataOutput = .init()
+    
+    override init() {
+        super.init()
+        output.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String : kCVPixelFormatType_32BGRA]
+        session.addInput(input)
+        session.addOutput(output)
+        session.connections[0].videoOrientation = .portrait
+        session.connections[0].isVideoMirrored = true
+    }
+    
+    func setSampleBufferDelegate(_ delegate: AVCaptureVideoDataOutputSampleBufferDelegate) {
+        output.setSampleBufferDelegate(delegate, queue: .main)
+    }
+    
+    func start() {
+        session.startRunning()
+    }
+    
+    func stop() {
+        session.stopRunning()
+    }
+}
---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Tomoya Hirano on 2020/04/02.
+//  Copyright © 2020 Tomoya Hirano. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate, TrackerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var toggleView: UISwitch!
+    let camera = Camera()
+    let tracker: HandTracker = HandTracker()!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        camera.setSampleBufferDelegate(self)
+        camera.start()
+        tracker.startGraph()
+        tracker.delegate = self
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)
+        tracker.processVideoFrame(pixelBuffer)
+        
+        DispatchQueue.main.async {
+            if !self.toggleView.isOn {
+                self.imageView.image = UIImage(ciImage: CIImage(cvPixelBuffer: pixelBuffer!))
+            }
+        }
+    }
+    
+    func handTracker(_ handTracker: HandTracker!, didOutputLandmarks landmarks: [Landmark]!) {
+        print(landmarks)
+    }
+    
+    func handTracker(_ handTracker: HandTracker!, didOutputPixelBuffer pixelBuffer: CVPixelBuffer!) {
+        DispatchQueue.main.async {
+            if self.toggleView.isOn {
+                self.imageView.image = UIImage(ciImage: CIImage(cvPixelBuffer: pixelBuffer))
+            }
+        }
+    }
+}
